338326c74fe7b0da1410a92a85bbab4d4566f0a0
Traceton Timmerman <traceton.timmerman@gmail.com>
2021-08-14 22:44:42 -0400

    removed generate file

--- spec/tailwind_for_ruby_spec.rb
+++ spec/tailwind_for_ruby_spec.rb
@@ -3,10 +3,6 @@ RSpec.describe TailwindForRuby do
     expect(TailwindForRuby::VERSION).not_to be nil
   end
 
-  it "returns test print good" do
-    expect(TailwindForRuby::Intro.TestPrint()).to eq("test print good")
-  end  
-
   it "does something useful" do
     expect(false).to eq(true)
   end
