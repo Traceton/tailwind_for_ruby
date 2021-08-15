d94cab71740833f160f7f7adb4ca2d97f578256c
Traceton Timmerman <traceton.timmerman@gmail.com>
2021-08-14 22:14:33 -0400

    working useless test

--- lib/tailwind_for_ruby/generate.rb
+++ lib/tailwind_for_ruby/generate.rb
@@ -1,5 +1,9 @@
 require "thor"
 
 module TailwindForRuby
-    
+    class Intro
+        def self.TestPrint
+             "test print good"
+        end     
+    end    
 end    
--- spec/tailwind_for_ruby_spec.rb
+++ spec/tailwind_for_ruby_spec.rb
@@ -3,6 +3,10 @@ RSpec.describe TailwindForRuby do
     expect(TailwindForRuby::VERSION).not_to be nil
   end
 
+  it "returns test print good" do
+    expect(TailwindForRuby::Intro.TestPrint()).to eq("test print good")
+  end  
+
   it "does something useful" do
     expect(false).to eq(true)
   end
