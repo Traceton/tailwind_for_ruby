c286b2b0c3fd910eba5c97582825d3ef9f80005b
Traceton Timmerman <traceton.timmerman@gmail.com>
2021-08-15 23:31:04 -0400

    small progress

--- lib/tailwind_for_ruby/generators/view.rb
+++ lib/tailwind_for_ruby/generators/view.rb
@@ -4,20 +4,24 @@ module TailwindForRuby
     module Generators
         class View < Thor::Group
             include Thor::Actions
-            argument :command_name, :type => :string
-            argument :model_name, :type => :string
+            argument :tailwind_ui_name, :type => :string
             argument :model_attributes, :type => :string
 
               def self.source_root
-                File.dirname(__FILE__) + "/view"
+                File.dirname(__FILE__) + "/view/templates"
               end
 
               def create_directory
-                empty_directory("generated_Views")
+                empty_directory("generated_tailwind_components")
               end
               
               def generate_view
-                template("view.txt", "generated_Views/#{model_name}s.html.erb")
+                ### Forms
+                ## Input Groups
+                # input_with_label
+                template("forms/input_with_label.txt", "generated_tailwind_components/#{tailwind_ui_name}s.html.erb") if tailwind_ui_name == "tailwind_input_with_label"
+                # input_with_label_and_help_text
+                template("forms/input_with_label_and_help_text.txt", "generated_tailwind_components/#{tailwind_ui_name}s.html.erb") if tailwind_ui_name == "input_with_label_and_help_text"
               end  
 
         end
