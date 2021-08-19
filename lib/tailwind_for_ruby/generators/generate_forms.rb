2fc3b9d99ac770222a7ed876444580b41376975e
Traceton Timmerman <traceton.timmerman@gmail.com>
2021-08-19 00:02:23 -0400

    small testing progress

--- lib/tailwind_for_ruby/generators/generate_forms.rb
+++ lib/tailwind_for_ruby/generators/generate_forms.rb
@@ -1,8 +1,12 @@
+
+
 module Generate_Forms
     def generate_forms(tailwind_ui_component,model_name,*model_attributes)
         # puts "tailwind ui component ---> #{tailwind_ui_component}"
         # puts "model name ---> #{model_name}"
         # puts "model attributes ---> #{model_attributes}"
-        final_form = "tailwind ui component ---> #{tailwind_ui_component} and model name ---> #{model_name} and model attributes ---> #{model_attributes}"
-      end
+        # final_form = "tailwind ui component ---> #{tailwind_ui_component} and model name ---> #{model_name} and model attributes ---> #{model_attributes}"
+        final_form = "<h1 class\=\"bg-gray-500\"> #{model_attributes} </h1>"
+
+    end
 end
