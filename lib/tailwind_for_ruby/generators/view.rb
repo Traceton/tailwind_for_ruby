9f6e18d97cf9faef1341114eaecc6988f5d4af13
Traceton Timmerman <traceton.timmerman@gmail.com>
2021-08-18 22:25:07 -0400

    small progress toward working generator

--- lib/tailwind_for_ruby/generators/view.rb
+++ lib/tailwind_for_ruby/generators/view.rb
@@ -4,7 +4,8 @@ module TailwindForRuby
     module Generators
         class View < Thor::Group
             include Thor::Actions
-            argument :tailwind_ui_name, :type => :string
+            argument :tailwind_ui_component, :type => :string
+            argument :model_name, :type => :string
             argument :model_attributes, :type => :string
 
               def self.source_root
@@ -13,16 +14,17 @@ module TailwindForRuby
 
               def create_directory
                 empty_directory("generated_tailwind_components")
-                puts model_attributes
+                puts "model name ---> #{model_name}"
+                puts "model attributes ---> #{model_attributes}"
               end
               
               def generate_view
                 ### Forms
                 ## Input Groups
                 # input_with_label
-                # template("forms/input_with_label.txt", "generated_tailwind_components/#{tailwind_ui_name}s.html.erb") if tailwind_ui_name == "input_with_label"
+                # template("forms/input_with_label.txt", "generated_tailwind_components/#{tailwind_ui_component}s.html.erb") if tailwind_ui_component == "input_with_label"
                 # input_with_label_and_help_text
-                # template("forms/input_with_label_and_help_text.txt", "generated_tailwind_components/#{tailwind_ui_name}s.html.erb") if tailwind_ui_name == "input_with_label_and_help_text"
+                # template("forms/input_with_label_and_help_text.txt", "generated_tailwind_components/#{tailwind_ui_component}s.html.erb") if tailwind_ui_component == "input_with_label_and_help_text"
               end  
               
         end
