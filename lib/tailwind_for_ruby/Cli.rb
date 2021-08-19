41f5aa9135eaff401bc7a9e5bd749bd37dda870b
Traceton Timmerman <traceton.timmerman@gmail.com>
2021-08-19 10:56:39 -0400

    small changes

--- lib/tailwind_for_ruby/Cli.rb
+++ lib/tailwind_for_ruby/Cli.rb
@@ -6,8 +6,8 @@ require 'tailwind_for_ruby/generator/ui'
 module TailwindForRuby
   class Cli < Thor
     desc "generate", "Generates different components"
-    def generate(tailwind_ui_component,model_name,*model_attributes)
-        TailwindForRuby::Generator_controller::Ui.start([ tailwind_ui_component,model_name,model_attributes])
+    def generate(component_to_generate,tailwind_ui_component,model_name,*model_attributes)
+        TailwindForRuby::Generator_controller::Ui.start([tailwind_ui_component,model_name,model_attributes]) if component_to_generate == "ui"
     end
   end
 end
