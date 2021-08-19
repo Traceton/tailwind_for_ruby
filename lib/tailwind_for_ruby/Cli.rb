2159802610696dffe288df65f99f9059bffed1f3
Traceton Timmerman <traceton.timmerman@gmail.com>
2021-08-18 23:26:43 -0400

    small progress

--- lib/tailwind_for_ruby/Cli.rb
+++ lib/tailwind_for_ruby/Cli.rb
@@ -6,7 +6,7 @@ require 'tailwind_for_ruby/generators/view'
 module TailwindForRuby
   class Cli < Thor
     desc "generate", "Generates a html.erb view"
-    def generate( tailwind_ui_component,model_name,*model_attributes)
+    def generate(tailwind_ui_component,model_name,*model_attributes)
         TailwindForRuby::Generators::View.start([ tailwind_ui_component,model_name,model_attributes])
     end
   end
--- lib/tailwind_for_ruby/generators/view.rb
+++ lib/tailwind_for_ruby/generators/view.rb
@@ -14,15 +14,16 @@ module TailwindForRuby
 
               def create_directory
                 empty_directory("generated_tailwind_components")
-                puts "model name ---> #{model_name}"
-                puts "model attributes ---> #{model_attributes}"
+                # puts "model name ---> #{model_name}"
+                # puts "model attributes ---> #{model_attributes}"
               end
               
               def generate_view
+                generate_forms(tailwind_ui_component,model_name,*model_attributes)
                 ### Forms
                 ## Input Groups
                 # input_with_label
-                template("forms/input_with_label.txt", "generated_tailwind_components/#{tailwind_ui_component}s.html.erb") if tailwind_ui_component == "input_with_label"
+                # template("forms/input_with_label.txt", "generated_tailwind_components/#{tailwind_ui_component}s.html.erb") if tailwind_ui_component == "input_with_label"
                 # input_with_label_and_help_text
                 # template("forms/input_with_label_and_help_text.txt", "generated_tailwind_components/#{tailwind_ui_component}s.html.erb") if tailwind_ui_component == "input_with_label_and_help_text"
               end  
@@ -30,3 +31,11 @@ module TailwindForRuby
         end
     end
 end    
+
+
+    def generate_forms(tailwind_ui_component,model_name,*model_attributes)
+      puts "tailwind ui component ---> #{tailwind_ui_component}"
+      puts "model name ---> #{model_name}"
+      puts "model attributes ---> #{model_attributes}"
+    end
+
