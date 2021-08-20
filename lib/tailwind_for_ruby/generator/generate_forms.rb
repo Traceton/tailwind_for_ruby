839842562b32409c1a8b835ad92f3d3eef31e5cc
Traceton Timmerman <traceton.timmerman@gmail.com>
2021-08-19 23:05:57 -0400

    small progress

--- lib/tailwind_for_ruby/generator/generate_forms.rb
+++ lib/tailwind_for_ruby/generator/generate_forms.rb
@@ -1,4 +1,4 @@
-
+require "CGI"
 
 module Generate_Forms
     def generate_forms(tailwind_ui_component,model_name,*model_attributes)
@@ -6,7 +6,9 @@ module Generate_Forms
         # puts "model name ---> #{model_name}"
         # puts "model attributes ---> #{model_attributes}"
         # final_form = "tailwind ui component ---> #{tailwind_ui_component} and model name ---> #{model_name} and model attributes ---> #{model_attributes}"
-        final_form = "<h1 class\=\"bg-gray-500\"> #{model_attributes} </h1>"
 
+        model_attributes.map { |model_attribute| puts CGI.unescape('<h1 class="bg-gray-500"> <%= model_attributes %> </h1>') }
+
+        final_form = CGI.unescape('<h1 class="bg-gray-500"> #{model_attributes} </h1>')
     end
 end
