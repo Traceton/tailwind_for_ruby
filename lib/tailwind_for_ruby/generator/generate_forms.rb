9e5cb660da3c2ccb926c011171c3609ef2293202
Traceton Timmerman <traceton.timmerman@gmail.com>
2021-08-23 13:47:36 -0400

    more progress

--- lib/tailwind_for_ruby/generator/generate_forms.rb
+++ lib/tailwind_for_ruby/generator/generate_forms.rb
@@ -10,13 +10,16 @@ module Generate_Forms
 
         def generate_input_with_label(model_name,*model_attributes)
             input_with_label = []
-            model_attributes.map { |model_attribute|  @final_form.push("
+            model_attributes.map do |model_attribute|  
+                attribute_name = model_attribute.split(":")[0]
+                @final_form.push("
                 <div>
-                <label for='#{model_attribute}' class='block text-sm font-medium text-gray-700'>#{model_attribute}</label>
+                <label for='#{attribute_name}' class='block text-sm font-medium text-gray-700'>#{attribute_name}</label>
                 <div class='mt-1'>
-                    <input type='text' name='#{model_attribute}' id='#{model_attribute}' class='shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md'>
+                    <input type='text' name='#{attribute_name}' id='#{attribute_name}' class='shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md'>
                 </div>
-                </div>") }
+                </div>") 
+            end    
         end
 
         generate_input_with_label(model_name,*model_attributes) if tailwind_ui_component == "input_with_label"
