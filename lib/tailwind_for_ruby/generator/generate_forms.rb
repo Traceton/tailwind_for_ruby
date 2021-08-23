2fe1b87d71b8549f6c6adc42d4a399c90059f1a8
Traceton Timmerman <traceton.timmerman@gmail.com>
2021-08-23 13:26:41 -0400

    small progress on generate input with label

--- lib/tailwind_for_ruby/generator/generate_forms.rb
+++ lib/tailwind_for_ruby/generator/generate_forms.rb
@@ -10,7 +10,13 @@ module Generate_Forms
 
         def generate_input_with_label(model_name,*model_attributes)
             input_with_label = []
-            model_attributes.map { |model_attribute|  @final_form.push("<h1 class='bg-gray-500'>  #{model_attribute}  </h1>") }
+            model_attributes.map { |model_attribute|  @final_form.push("
+                <div>
+                <label for='#{model_attribute}' class='block text-sm font-medium text-gray-700'>#{model_attribute}</label>
+                <div class='mt-1'>
+                    <input type='text' name='#{model_attribute}' id='#{model_attribute}' class='shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md'>
+                </div>
+                </div>") }
         end
 
         generate_input_with_label(model_name,*model_attributes) if tailwind_ui_component == "input_with_label"
