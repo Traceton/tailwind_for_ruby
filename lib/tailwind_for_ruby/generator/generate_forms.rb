0404dd8c7796b45ca8e108605b419e570bff2ede
Traceton Timmerman <traceton.timmerman@gmail.com>
2021-08-23 16:05:18 -0400

    small progress

--- lib/tailwind_for_ruby/generator/generate_forms.rb
+++ lib/tailwind_for_ruby/generator/generate_forms.rb
@@ -12,20 +12,22 @@ module Generate_Forms
             input_with_label = []
             model_attributes.map do |model_attribute|  
                 attribute_name = model_attribute.split(":")[0]
-                @final_form.push("
+                attribute = "
                 <div>
                 <label for='#{attribute_name}' class='block text-sm font-medium text-gray-700'>#{attribute_name}</label>
                 <div class='mt-1'>
                     <input type='text' name='#{attribute_name}' id='#{attribute_name}' class='shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md'>
                 </div>
-                </div>") 
+                </div>"
+                puts attribute 
+                @final_form.push(attribute) 
             end    
         end
 
         #1 ApplicationUI/Forms/InputGroups/input_with_label
         generate_input_with_label(model_name,*model_attributes) if tailwind_ui_component == "input_with_label"
 
-
+    
 
 
         # component returned to the "UI" class
