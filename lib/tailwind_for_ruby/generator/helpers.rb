380b0793ef4b5ed231ca8004ce74c13cf3195873
Traceton Timmerman <traceton.timmerman@gmail.com>
2021-08-24 16:49:26 -0400

    progresson global variable for list of tailwind components

--- lib/tailwind_for_ruby/generator/helpers.rb
+++ lib/tailwind_for_ruby/generator/helpers.rb
@@ -1,20 +1,9 @@
 # updated aug 24, 2021
+require_relative "tailwind_components.rb"
 module Helpers
     def determine_tailwind_group(tailwind_ui_component)
-        
-        # application shells/
-        #  stacked_layouts,sidebar_layouts,multi_column_layouts
-        application_shells = %w[ stacked_layouts: %w[ light_nav_with_bottom_border light_nav_on_gray_background
-         dark_nav_with_white_page_header branded_nav_with_compact_white_page_header dark_nav_with_overlap
-          brand_nav_with_overlap branded_nav_with_white_page_header dark_nav_with_compact_white_page_header
-          two-row_navigation_with_overlap ]]
-
-        forms = %w[ input_groups: %w[ input_with_label input_with_label_and_help_text input_with_validation_error ],
-        toggles: %w[ simple_toggle short_toggle toggle_with_icon with_left_label_and_description with_right_label ],
-        action_panels: %w[ simple with_link with_button_on_right with_button_at_top_right with_toggle with_input simple_well with_well ] ]  
-
-        tailwind_group = "application_shells" if application_shells.include?(tailwind_ui_component)
-        tailwind_group = "forms" if forms.include?(tailwind_ui_component)
+        tailwind_group = "application_shells" if $application_shells.include?(tailwind_ui_component)
+        tailwind_group = "forms" if $forms.include?(tailwind_ui_component)
 
         if tailwind_group != nil
             puts tailwind_group
---
+++ lib/tailwind_for_ruby/generator/tailwind_components.rb
@@ -0,0 +1,10 @@
+
+
+    $application_shells = %w[ stacked_layouts: %w[ light_nav_with_bottom_border light_nav_on_gray_background
+    dark_nav_with_white_page_header branded_nav_with_compact_white_page_header dark_nav_with_overlap
+    brand_nav_with_overlap branded_nav_with_white_page_header dark_nav_with_compact_white_page_header
+    two-row_navigation_with_overlap ]]
+
+    $forms = %w[ input_groups: %w[ input_with_label input_with_label_and_help_text input_with_validation_error ],
+    toggles: %w[ simple_toggle short_toggle toggle_with_icon with_left_label_and_description with_right_label ],
+    action_panels: %w[ simple with_link with_button_on_right with_button_at_top_right with_toggle with_input simple_well with_well ] ]  
