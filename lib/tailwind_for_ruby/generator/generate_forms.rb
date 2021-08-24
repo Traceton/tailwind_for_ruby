a72aac4e4317fef0fa4d58bd9acae954abcdf974
Traceton Timmerman <traceton.timmerman@gmail.com>
2021-08-23 20:04:56 -0400

    more progresson generating

--- lib/tailwind_for_ruby/generator/generate_forms.rb
+++ lib/tailwind_for_ruby/generator/generate_forms.rb
@@ -1,33 +1,31 @@
-require "CGI"
+require 'CGI'
 
 module Generate_Forms
-    def generate_forms(tailwind_ui_component,model_name,*model_attributes)
-        # initiates the component that will eventually be returned to the "UI" class
-        @final_form = []
+  def generate_forms(tailwind_ui_component, model_name, *model_attributes)
+    # initiates the component that will eventually be returned to the "UI" class
+    @final_form = []
 
-
-
-        #1 ApplicationUI/Forms/InputGroups/input_with_label
-        def generate_input_with_label(model_name,*model_attributes)
-            input_with_label = []
-            model_attributes.map do |model_attribute|  
-                attribute_name = model_attribute.split(":")[0]
-                attribute = "
+    # 1 ApplicationUI/Forms/InputGroups/input_with_label
+    def generate_input_with_label(_model_name, *model_attributes)
+      input_with_label = []
+      model_attributes.map do |model_attribute|
+        attribute_name = model_attribute.split(':')[0]
+        attribute = "
                 <div>
                     <label for='#{attribute_name}' class='block text-sm font-medium text-gray-700'>#{attribute_name}</label>
                     <div class='mt-1'>
-                        <input type='text' name='#{attribute_name}' id='#{attribute_name}' 
+                        <input type='text' name='#{attribute_name}' id='#{attribute_name}'
                         class='shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md'>
                     </div>
                 </div>"
-                @final_form.push(attribute) 
-            end    
-        end
-
-        #1 ApplicationUI/Forms/InputGroups/input_with_label
-        generate_input_with_label(model_name,*model_attributes) if tailwind_ui_component == "input_with_label"
-
-        # component returned to the "UI" class
-        return @final_form.to_s.gsub("[", "").gsub("]", "").gsub("\\n","").gsub("\"","").gsub("\,","")
+        @final_form.push(attribute)
+      end
     end
+
+    # 1 ApplicationUI/Forms/InputGroups/input_with_label
+    generate_input_with_label(model_name, *model_attributes) if tailwind_ui_component == 'input_with_label'
+
+    # component returned to the "UI" class
+    @final_form.to_s.gsub('[', '').gsub(']', '').gsub('\\n', '').gsub('"', '').gsub("\,", '')
+  end
 end
--- lib/tailwind_for_ruby/generator/ui.rb
+++ lib/tailwind_for_ruby/generator/ui.rb
@@ -1,36 +1,38 @@
-require "thor/group"
-require_relative "generate_forms"
+require 'thor/group'
+require_relative 'generate_forms'
 
 module TailwindForRuby
-    module Generator_controller
-        class Ui < Thor::Group
-            include Generate_Forms
-            include Thor::Actions
-            argument :tailwind_ui_component, :type => :string
-            argument :model_name, :type => :string
-            argument :model_attributes, :type => :array
+  module Generator_controller
+    class Ui < Thor::Group
+      include Generate_Forms
+      include Thor::Actions
+      argument :tailwind_ui_component, type: :string
+      argument :model_name, type: :string
+      argument :model_attributes, type: :array
 
-              def self.source_root
-                File.dirname(__FILE__) + "/view/templates"
-              end
+      def self.source_root
+        File.dirname(__FILE__) + '/view/templates'
+      end
 
-              def create_directory
-                empty_directory("generated_tailwind_components")
-                # puts "model name ---> #{model_name}"
-                # puts "model attributes ---> #{model_attributes}"
-              end
-              
-              def generate_view
-                tailwind_ui_form_components = ["input_with_label","input_with_label_and_help_text"]
+      def create_directory
+        empty_directory('generated_tailwind_components')
+        # puts "model name ---> #{model_name}"
+        # puts "model attributes ---> #{model_attributes}"
+      end
 
-                final_form = generate_forms(tailwind_ui_component,model_name,*model_attributes) if tailwind_ui_form_components.include?(tailwind_ui_component) 
-                
-                # puts final_form if final_form != nil
+      def generate_view
+        tailwind_ui_form_components = %w[input_with_label input_with_label_and_help_text]
 
-                File.write("generated_tailwind_components/#{tailwind_ui_component}-#{model_name}.html.erb", final_form) if final_form != nil
-   
-              end  
-              
+        if tailwind_ui_form_components.include?(tailwind_ui_component)
+          final_component = generate_forms(tailwind_ui_component, model_name,
+                                           *model_attributes)
         end
+
+        unless final_component.nil?
+          File.write("generated_tailwind_components/#{tailwind_ui_component}-#{model_name}.html.erb",
+                     final_component)
+        end
+      end
     end
-end    
+  end
+end
