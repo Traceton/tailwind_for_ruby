e0fa3f3048f8837458d56296dcc3b7bdd68d10af
Traceton Timmerman <traceton.timmerman@gmail.com>
2021-08-24 13:44:35 -0400

    progress

---
+++ lib/tailwind_for_ruby/generator/helpers.rb
@@ -0,0 +1,9 @@
+# updated aug 24, 2021
+module Helpers
+    def determine_input_type(tailwind_ui_component)
+        # application shells/
+        #  stacked_layouts,sidebar_layouts,multi_column_layouts
+        application_shells = %w[]
+    end
+   
+end
