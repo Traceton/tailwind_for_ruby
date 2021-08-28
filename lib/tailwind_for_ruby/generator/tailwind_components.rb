b2a49abe0d773ec96917f633e0e3a809681515b7
Traceton Timmerman <traceton.timmerman@gmail.com>
2021-08-28 17:54:44 -0400

    converted tailwind components to hashes

--- lib/tailwind_for_ruby/generator/tailwind_components.rb
+++ lib/tailwind_for_ruby/generator/tailwind_components.rb
@@ -1,18 +1,18 @@
 
 
-    $test_object = {"group1" => %w[this is group one], "group2" => %w[this is group 2]}
+    # $test_object = {"group1" => %w[this is group one], "group2" => %w[this is group 2]}
 
-    $application_shells = %w[ stacked_layouts: %w[ light_nav_with_bottom_border light_nav_on_gray_background
+    $application_shells = { "stacked_layouts" => %w[ light_nav_with_bottom_border light_nav_on_gray_background
     dark_nav_with_white_page_header branded_nav_with_compact_white_page_header dark_nav_with_overlap
     brand_nav_with_overlap branded_nav_with_white_page_header dark_nav_with_compact_white_page_header
-    two-row_navigation_with_overlap ]]
+    two-row_navigation_with_overlap ]}
 
-    $forms = %w[ input_groups: %w[ input_with_label input_with_label_and_help_text input_with_validation_error input_with_hidden_label
+    $forms = { "input_groups" => %w[ input_with_label input_with_label_and_help_text input_with_validation_error input_with_hidden_label
      input_with_corner_hint input_with_leading_icon inpput_with_trailing_icon input_with_add-on input_with_inline_add-on
       input_with_inline_leading_and_trailing_add-ons input_with_inline_leading_dropdown input_with_inline_leading_add-on_and_trailing_dropdown
        input_with_leading_icon_and_trailing_button input_with_shared_borders input_with_insert_label inputs_with_inset_labels_and_shared_borders
         input_with_overlapping_label input_with_pill_shape input_with_gray_background_and_bottom_border input_with_keyboard_shortcut ], 
-    select_menus: %w[ simple_native simple_custom custom_with_check_on_left custom_with_status_indicator custom_with_avatar with_secondary_text
+    "select_menus" => %w[ simple_native simple_custom custom_with_check_on_left custom_with_status_indicator custom_with_avatar with_secondary_text
      branded_with_supported_text ],    
-    toggles: %w[ simple_toggle short_toggle toggle_with_icon with_left_label_and_description with_right_label ],
-    action_panels: %w[ simple with_link with_button_on_right with_button_at_top_right with_toggle with_input simple_well with_well ] ]  
+    "toggles" => %w[ simple_toggle short_toggle toggle_with_icon with_left_label_and_description with_right_label ],
+    "action_panels" => %w[ simple with_link with_button_on_right with_button_at_top_right with_toggle with_input simple_well with_well ] }  
