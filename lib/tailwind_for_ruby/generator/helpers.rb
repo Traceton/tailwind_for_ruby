26080d32cbcb792538e9d6db549a86dc29bc1c51
Traceton Timmerman <traceton.timmerman@gmail.com>
2021-08-28 22:02:42 -0400

    small progress working on tailwind components

--- lib/tailwind_for_ruby/generator/helpers.rb
+++ lib/tailwind_for_ruby/generator/helpers.rb
@@ -4,12 +4,21 @@ require_relative "tailwind_components.rb"
 module Helpers
 
     def determine_tailwind_group(tailwind_ui_component)
-        tailwind_group = "application_shells" if $application_shells.include?(tailwind_ui_component)
-        tailwind_group = "forms" if $forms.include?(tailwind_ui_component)
+        # tailwind_group = "application_shells" if $application_shells.include?(tailwind_ui_component)
+        # tailwind_group = "forms" if $forms.include?(tailwind_ui_component)
+        @tailwind_group
+        $application_shells.map do |ui_group| 
+            ui_group.map do |ui_component| 
+                if ui_component == tailwind_ui_component
+                    @tailwind_group = ui_group
+                end
+            end    
+        end    
 
-        if tailwind_group != nil
-            puts tailwind_group
-            return tailwind_group
+
+        if @tailwind_group != nil
+            puts @tailwind_group
+            return @tailwind_group
         else
             puts "tailwind ui component not found in determine_tailwind_group function"
         end
